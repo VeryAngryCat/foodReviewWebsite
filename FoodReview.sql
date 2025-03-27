a36e14e2ba6a3a9fa8584ee0a68bdbaea55d3c19
VeryAngry Cat <iKoroleva2006@gmail.com>
2025-03-27 17:29:13 +0400

    Added values to restaurants

--- FoodReview.sql
+++ FoodReview.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 27, 2025 at 01:05 PM
+-- Generation Time: Mar 27, 2025 at 02:07 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -20,23 +20,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `FoodReview`
 --
-
--- --------------------------------------------------------
---
--- Delete these rows after project is done
---
 SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS `Cuisine`;
-DROP TABLE IF EXISTS `DietaryPreference`;
-DROP TABLE IF EXISTS `Dish`;
-DROP TABLE IF EXISTS `FavouriteDish`;
-DROP TABLE IF EXISTS `FavouriteRestaurant`;
-DROP TABLE IF EXISTS `Restaurant`;
-DROP TABLE IF EXISTS `RestaurantCuisine`;
-DROP TABLE IF EXISTS `Reviews`;
-DROP TABLE IF EXISTS `UserPreference`;
-DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS Cuisine;
+DROP TABLE IF EXISTS DietaryPreference;
+DROP TABLE IF EXISTS Dish;
+DROP TABLE IF EXISTS FavouriteDish;
+DROP TABLE IF EXISTS FavouriteRestaurant;
+DROP TABLE IF EXISTS Restaurant;
+DROP TABLE IF EXISTS RestaurantCuisine;
+DROP TABLE IF EXISTS Reviews;
+DROP TABLE IF EXISTS UserPreference;
+DROP TABLE IF EXISTS Users;
 SET FOREIGN_KEY_CHECKS = 1;
+-- --------------------------------------------------------
 --
 -- Table structure for table `Cuisine`
 --
@@ -98,6 +94,144 @@ CREATE TABLE `Restaurant` (
   `location` varchar(100) DEFAULT NULL,
   `operationStatus` varchar(40) DEFAULT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+--
+-- Dumping data for table `Restaurant`
+--
+
+INSERT INTO `Restaurant` (
+    `restaurantID`,
+    `name`,
+    `location`,
+    `operationStatus`
+  )
+VALUES (1, 'MacDonalds', 'New York, 15th Avenue', 'Open'),
+  (
+    2,
+    'Mamma Mias Desserts',
+    'Italy, Liguria, Cinque Terre',
+    'Open'
+  ),
+  (
+    3,
+    'Mario Pizza Place',
+    'Australia, Dangerville, Rattlesnake st.',
+    'Temporarily closed'
+  ),
+  (4, 'Chin chin', 'Shanghai, 14P', 'Open'),
+  (
+    5,
+    'Indo-Asian Buffet',
+    'UK, Manchester, Hottea st.',
+    'Open'
+  ),
+  (
+    6,
+    'Shi-sh kebab',
+    'India, Andhra Pradesg, Greams Road',
+    'Open'
+  ),
+  (
+    7,
+    'Rheas saladeria - Southern Branch',
+    'Texas, Yorkshire, Lune st.',
+    'Permanently closed'
+  ),
+  (
+    8,
+    'Rheas saladeria - Northern Branch',
+    'Texas, Yorkshire, 25th st.',
+    'Open'
+  ),
+  (
+    9,
+    'See Food Eat Food: Best Fish and Lobster Place',
+    'Spain, Costa Brava, Calle Verde',
+    'Open'
+  ),
+  (
+    10,
+    'Isthis Theright Place',
+    'Alabama, Whoknowswhere Road',
+    'Open'
+  ),
+  (
+    11,
+    'Chop House',
+    'Brazil, Rio de Janeiro, Avenida Vieira Souto',
+    'Open'
+  ),
+  (
+    12,
+    'Sakura Asian Bistro',
+    'Alaska, Anchorage, Frostbite Boulevard',
+    'Permanantly closed'
+  ),
+  (
+    13,
+    'Chisou Nishi Kenichi',
+    'Japan, Kyoto, Sannenzaka.',
+    'Open'
+  ),
+  (
+    14,
+    'Song Fa Bak Kut Teh',
+    'Singapore, New Bridge Road',
+    'Temporarily closed'
+  ),
+  (
+    15,
+    'Wise Kwai Thai Streetfood',
+    'Thailand, Rongmuang',
+    'Open'
+  ),
+  (
+    16,
+    'No Imagination',
+    'The Default City, Street st.',
+    'Open'
+  ),
+  (
+    17,
+    'Dunedin New Zealand Eats',
+    'San Diego, 3501 30th',
+    'Open'
+  ),
+  (
+    18,
+    'Mamas Tasty Meatballs',
+    'Italy, Milan, Gelato Boulevard',
+    'Temporarily closed'
+  ),
+  (
+    19,
+    'Wan Li',
+    'Beijing, Renaissance Beijing Wangfujing Hotel',
+    'Open'
+  ),
+  (
+    20,
+    'Goubuli Restaurant',
+    'China, Tuanjin, Shandong Road',
+    'Open'
+  ),
+  (
+    21,
+    'WelcomCafe Cambay',
+    'India, Vadodara',
+    'Open'
+  ),
+  (
+    22,
+    'Grasslovers',
+    'Namibia, Windhoek, Khomas st.',
+    'Open'
+  ),
+  (
+    23,
+    'Bebek Restaurant',
+    'Saudi Arabia, Tabuk, Adenia Lane',
+    'Open'
+  );
 -- --------------------------------------------------------
 --
 -- Table structure for table `RestaurantCuisine`
@@ -140,35 +274,158 @@ CREATE TABLE `Users` (
   `userID` int(11) NOT NULL,
   `firstName` varchar(25) DEFAULT NULL,
   `lastName` varchar(35) DEFAULT NULL,
-  `email` varchar(35) DEFAULT NULL
+  `email` varchar(35) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `userPassword` varchar(20) DEFAULT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 --
 -- Dumping data for table `Users`
 --
 
-INSERT INTO `Users` (`userID`, `firstName`, `lastName`, `email`)
-VALUES (1, 'Alan', 'Poe', 'aPope@holicorp.com'),
-  (2, 'Paul', 'Brook', 'pBrook10@company.com'),
-  (3, 'Sally', 'Tims', 'sTim1980@gmail.com'),
-  (4, 'Ron', 'White', 'ronny96@me.com'),
-  (5, 'Gina', 'Lopez', 'lPezto3459@company.com'),
-  (6, 'Donald', 'Morris', 'dMorris1990@gmail.com'),
-  (7, 'Olaf', 'Snow', 'letItSnow@gmail.com'),
-  (8, 'Tony', 'Stark', 'ironMan250@starkcorp.com'),
-  (9, 'Newt', 'Pearson', 'someone3ls3@company.com'),
-  (10, 'Remy', 'Cooks', 'remyRat55@company.com'),
-  (11, 'Steve', 'Bobs', 'sBobs4ever30@gmail.com'),
-  (12, 'Liam', 'King', 'liKing217@somecorp.com'),
+INSERT INTO `Users` (
+    `userID`,
+    `firstName`,
+    `lastName`,
+    `email`,
+    `username`,
+    `userPassword`
+  )
+VALUES (
+    1,
+    'Alan',
+    'Poe',
+    'aPope@holicorp.com',
+    'aPoe',
+    'aP138693'
+  ),
+  (
+    2,
+    'Paul',
+    'Brook',
+    'pBrook10@company.com',
+    'brookPaul',
+    'Qwerty123'
+  ),
+  (
+    3,
+    'Sally',
+    'Tims',
+    'sTim1980@gmail.com',
+    'foodCritic',
+    'fcT354kon'
+  ),
+  (
+    4,
+    'Ron',
+    'White',
+    'ronny96@me.com',
+    'Nom Lover',
+    's000pGone'
+  ),
+  (
+    5,
+    'Gina',
+    'Lopez',
+    'lPezto3459@company.com',
+    'MunchForLunch',
+    'PassWord1'
+  ),
+  (
+    6,
+    'Donald',
+    'Morris',
+    'dMorris1990@gmail.com',
+    'Donald Duck',
+    'catsAreLife42'
+  ),
+  (
+    7,
+    'Olaf',
+    'Snow',
+    'letItSnow@gmail.com',
+    'Olaf Snow',
+    'cancelSpring302'
+  ),
+  (
+    8,
+    'Tony',
+    'Stark',
+    'ironMan250@starkcorp.com',
+    'Iron Man',
+    'nukE333333'
+  ),
+  (
+    9,
+    'Newt',
+    'Pearson',
+    'someone3ls3@company.com',
+    'kkkkk2',
+    'sFgHii56249'
+  ),
+  (
+    10,
+    'Remy',
+    'Cooks',
+    'remyRat55@company.com',
+    'RatatouilleDaBest23',
+    ''
+  ),
+  (
+    11,
+    'Steve',
+    'Bobs',
+    'sBobs4ever30@gmail.com',
+    'AnAppleADayIsNotEnough',
+    'SBisB3456'
+  ),
+  (
+    12,
+    'Liam',
+    'King',
+    'liKing217@somecorp.com',
+    'CertifiedFoodie',
+    'fooD12345'
+  ),
   (
     13,
     'Volk',
     'Swagen',
-    'cars4Cheap3r@insurcorp.com'
+    'cars4Cheap3r@insurcorp.com',
+    'OilOilOil',
+    'le45hTT56'
   ),
-  (14, 'Andrea', 'LLowene', 'aLL23968@reple.com'),
-  (15, 'Gary', 'Rival', 'gRavel42@gmail.com'),
-  (16, 'Zach', 'Wood', 'zwood2003@company.com'),
-  (17, 'Mac', 'Bigs', 'bigMacs11@corp.com');
+  (
+    14,
+    'Andrea',
+    'LLowene',
+    'aLL23968@reple.com',
+    'Andrea<3Pizza',
+    'ALready8080'
+  ),
+  (
+    15,
+    'Gary',
+    'Rival',
+    'gRavel42@gmail.com',
+    'PokeCuisine',
+    'Hawk2ah2025'
+  ),
+  (
+    16,
+    'Zach',
+    'Wood',
+    'zwood2003@company.com',
+    'Zach2003',
+    'Zwood2003'
+  ),
+  (
+    17,
+    'Mac',
+    'Bigs',
+    'bigMacs11@corp.com',
+    'fastFoodIsLife',
+    'mGTree20'
+  );
 --
 -- Indexes for dumped tables
 --
@@ -255,7 +512,8 @@ MODIFY `dishID` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT for table `Restaurant`
 --
 ALTER TABLE `Restaurant`
-MODIFY `restaurantID` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `restaurantID` int(11) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 24;
 --
 -- AUTO_INCREMENT for table `Reviews`
 --
