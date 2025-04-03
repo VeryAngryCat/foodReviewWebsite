832333cb2633e7092f547764830cdbbf9b37a6c7
VeryAngry Cat <iKoroleva2006@gmail.com>
2025-04-03 16:43:45 +0400

    yoo hoo

--- public/login.php
+++ public/login.php
@@ -16,10 +16,11 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT * FROM Users"
-$result = mysqli_query($conn, $sql)
+$sql = "SELECT * FROM Users";
+$result = mysqli_query($conn, $sql);
 
 while ($row = mysqli_fetch_assoc($result)) {
-    echo "User: " . $row["username"] . " – Email: " . $row["email"] / <br>;
+    echo "User: " . $row["username"] . " – Email: " . $row["email"] . "<br>";
 }
+mysqli_close($conn);
 ?>
