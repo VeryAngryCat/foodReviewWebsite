7c5b1ea04b0f13fe95d034fbce071dec42a86f43
VeryAngry Cat <ik4001@hw.ac.uk>
2025-04-10 14:16:48 +0400

    comments

--- admin/adminLogin.php
+++ admin/adminLogin.php
@@ -24,12 +24,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         if (mysqli_num_rows($result) == 1) {
             $row = mysqli_fetch_assoc($result);
-            $storedPassword = $row["adminPassword"]; // This is solely for the prepopulated passwords, for demonstration (as they are not hashed). Otherwise, there would  be no need for this if else loop and it would  solely start from password_verify
-            if (password_verify($pword, $storedPassword)) {
+            $storedPassword = $row["adminPassword"]; // This is solely for the prepopulated passwords, for demonstration (as they are not hashed). Otherwise, there would  be no need for this if else loop and it would solely start from password_verify
+            if (password_verify($pword, $storedPassword)) { // For hashed passwords
                 $_SESSION['adminID'] = $row['adminID'];
-                header("Location: ../admin/dashboard.php");
+                header("Location: ../admin/dashboard.php"); // Move admin to dashboard
                 exit();
-            } else if ($pword === $storedPassword) { // For legacy plain-text passwords
+            } else if ($pword === $storedPassword) {
                 $_SESSION['adminID'] = $row['adminID'];
                 header("Location: ../admin/dashboard.php");
                 exit();
@@ -60,17 +60,18 @@ mysqli_close($conn);
 <body>
     <div class="container">
         <h2>Administrator Login</h2>
+        <!-- Data entry fields -->
         <form method="POST" action="adminLogin.php">
             <label for="username">Username</label>
             <input type="text" id="username" name="username" required>
             <label for="adminPassword">Password</label>
             <input type="password" id="adminPassword" name="adminPassword" required>
 
-            <input type="submit" value="Log in">
+            <input type="submit" value="Log in"> <!-- Submit button -->
         </form>
         <?php
             if ($error_message) {
-                echo "<p class='error'>$error_message</p>";
+                echo "<p class='error'>$error_message</p>"; // Displays error message if set in the php section
             }
         ?>
     </div>
